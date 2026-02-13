aa372040aa0040004689ec57e18db5f92fb44eab
auwal adam <kaseemauwal50@gmail.com>
2026-02-13 18:21:46 +0100

    fix: update

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
+
 import { Button } from '@/components/ui/button';
 
 defineProps<{
@@ -14,20 +15,24 @@ defineProps<{
 <template>
     <div v-if="links.length > 3" class="flex flex-wrap justify-center gap-1 mt-6">
         <template v-for="(link, key) in links" :key="key">
-            <div v-if="link.url === null" 
-                class="mr-1 mb-1 px-4 py-2 text-sm leading-4 text-gray-400 border rounded" 
-                v-html="link.label" 
+            <div
+                v-if="link.url === null"
+                class="mr-1 mb-1 px-4 py-2 text-sm leading-4 text-gray-400 border rounded"
+                v-html="link.label"
             />
-            <Link v-else 
-                class="mr-1 mb-1" 
+            <Link
+                v-else
+                class="mr-1 mb-1"
                 :href="link.url"
             >
-                <Button 
-                    :variant="link.active ? 'default' : 'outline'" 
+                <Button
+                    :variant="link.active ? 'default' : 'outline'"
                     size="sm"
-                    v-html="link.label"
-                />
+                >
+                    <span v-html="link.label"></span>
+                </Button>
             </Link>
         </template>
     </div>
 </template>
+
